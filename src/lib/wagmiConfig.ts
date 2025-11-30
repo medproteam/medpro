d6e9252a92f42d5534bc850a8ab24719121fb393
medproteam <infomedpro25@gmail.com>
2025-11-30 22:40:28 +0100

    Update wagmiConfig.ts

--- src/lib/wagmiConfig.ts
+++ src/lib/wagmiConfig.ts
@@ -1,11 +1,21 @@
 import { createConfig, http } from 'wagmi';
-import { injected } from 'wagmi/connectors';
+import { injected, walletConnect } from 'wagmi/connectors';
 import { campTestnet } from '@/config/campNetwork';
 
 export const wagmiConfig = createConfig({
   chains: [campTestnet],
   connectors: [
     injected({ shimDisconnect: true }),
+    walletConnect({
+      projectId: 'ba1146c85ab32c5ceea9cdefdcf5e187',
+      metadata: {
+        name: 'MEDPRO',
+        description: 'Decentralized Health Management Platform',
+        url: 'https://camedpro.vercel.app',
+        icons: ['https://medpro.app/medpro-logo.png']
+      },
+      showQrModal: true,
+    }),
   ],
   transports: {
     [campTestnet.id]: http(),
